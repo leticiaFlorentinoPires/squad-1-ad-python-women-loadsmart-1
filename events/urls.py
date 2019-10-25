369be3cf128607208bc8239fd2fcbb21ff22c357
carollzambelli <caroll.zambelli@gmail.com>
2019-10-24 21:15:33 -0300

    mudança para views

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path, re_path
 from rest_framework import routers
-
 from events import views
+from events.views import EventsListView, EventFilter, EventDetail
 
 router = routers.DefaultRouter()
 router.register(r'events', views.EventAPIViewSet)
@@ -9,7 +9,7 @@ router.register(r'agents', views.AgentAPIViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('', views.list_events, name='list_events'),
-    path('<str:envName>/', views.post_detail, name='post_detail'),
-    path('<int:event_id>', views.get_event, name='detail')
+    path('', EventsListView.as_view(), name='events-list'),
+    path('<str:envName>/', EventFilter.as_view(), name='filter_events'),
+    path('<int:event_id>', EventDetail.as_view(), name='detail')
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
 from django.db.models import QuerySet, Count
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.http import HttpResponseNotFound, HttpResponseBadRequest
 from rest_framework import viewsets
 
@@ -9,71 +11,39 @@ from .serializers import (
     AgentModelSerializer
 )
 
-def list_events(request):
-    """Return all events."""
-    #gerar frequencia
-    events = Event.objects.all()
 
-    result_group_by = list(Event.objects.values('agent_id','level').annotate(acount=Count('agent_id')))
-
-    print("resultado do group by \n")
-    # print(result_group_by)
-
-    event_count = list()
-    # print(result_group_by)
-    for group in result_group_by:
-        # print(group)
-        for event in events:
-            # print(dados)
-            if group['agent_id'] == event.agent.id and group['level']==event.level:
-                event_count.append({'event': event,
-                                    'agentId': group['agent_id'],
-                                    'count': group['acount'] })
+class EventsListView(ListView):
+    
+    paginate_by = 100
+    model = Event
 
 
-    # para passar para o "dropdown" de enviroments
-    enviroments = Agent.objects.values('env').distinct()
-    envReturn = list()
-    for env in enviroments:
-        envReturn.append(env['env'])
-    # print(event_count)
-    context = {
-        'events': event_count,
-        'env': envReturn,
-        'events_empty': []
-    }
-
-    return render(request, 'events/list.html', context=context)
-
-def get_event(request, event_id):
-    """Return event by a given id."""
-    if request.method == 'GET':
-        try:
-            event = Event.objects.get(pk=event_id)
-            context = {
-                'event': event,
+class EventFilter(DetailView):
+    
+    def get(self, request, envName):
+        
+        event =  Event.objects.get_queryset().filter(agent__env=envName)
+        context = {
+            'object_list': event,
             }
-            return render(request, 'events/detail.html', context=context)
-        except Event.DoesNotExist:
-            return HttpResponseNotFound("event not found")
-    return HttpResponseBadRequest("invalid action")
+        return render(request, 'events/event_list.html', context=context)
 
-def post_detail(request, envName):
-    query = Event.objects.get_queryset().filter(agent__env=envName)
-    context = {
-        'events': query
-    }
+class EventDetail(DetailView):
 
-    return render(request, 'events/list.html', context=context)
+    def get(self, request, event_id):
 
-
+        event = Event.objects.get(pk=event_id)
+        context = {
+            'event': event,
+            }
+        return render(request, 'events/detail.html', context=context)
 
 
 class AgentAPIViewSet(viewsets.ModelViewSet):
     queryset = Agent.objects.all()
     serializer_class = AgentModelSerializer
 
+
 class EventAPIViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventModelSerializer
-

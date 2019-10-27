03f65950803c7ae83658125f477fda08adfda7a4
leticiaFlorentinoPires <lepirescomp@gmail.com>
2019-10-26 21:38:55 -0300

    Added Dropdown button filter enviroment Added Dropdown button filter orderby Added Dropdown button filter search for Added Text field Added search button Added filter logic by enviroment Added filter logic by order by Added filter logic by search for Added filter logic by text to search for Added frequency to field "Eventos" Added ENV_CHOICES to model Agent Modified LEVEL_CHOICES to support order by

--- events/views.py
+++ events/views.py
@@ -22,22 +22,65 @@ class EventsListView(ListView):
 class EventFilter(ListView):
     model = Event
     template_name = '../templates/events/event_list.html'
+    env = None
+    orderBy = None
+    search_for = None
+    pesquisa_text = None
+    retorno_query = None
+    ordering = ["level"]
 
+    def calcula_frequencia(self):
+        event_group_by = self.retorno_query.values('agent__id', 'level').annotate(freq=Count('agent_id'))
+        dict_frequencia = dict()
+        for grupo in event_group_by:
+            for event in Event.objects.all():
+                if grupo['agent__id'] == event.agent_id and grupo['level'] == event.level:
+                      dict_frequencia[event.id] = (event,grupo['freq'],grupo['level'])
+        return dict_frequencia
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(EventFilter, self).get_context_data(**kwargs)
-        teste= Event.objects.values('agent__env').distinct()
-        context['dropdown_list'] = ["dev","production","homologacao"]
+        context['dropdown_list'] = ["dev","producao","homologacao"]
         print(Event.objects.values('agent__env').distinct())
         return context
 
+    def get_queryset(self):
+        print("resultado do get_queryset")
+        self.retorno_query = Event.objects.all()
+        if self.env is not None and self.env!="env":
+           self.retorno_query= self.retorno_query.filter(agent__env=self.env)
+        if self.search_for is not None and self.search_for!="buscaCampo":
+            if self.pesquisa_text is not None and not self.pesquisa_text == "":
+                if self.search_for == "level":
+                    self.retorno_query = self.retorno_query.filter(level=self.pesquisa_text)
+                elif self.search_for == "descricao":
+                    self.retorno_query = self.retorno_query.filter(data=self.pesquisa_text)
+                elif self.search_for == "origem":
+                    print("origem")
+                    print(self.pesquisa_text)
+                    self.retorno_query = self.retorno_query.filter(agent__address=self.pesquisa_text)
+
+        if self.orderBy is not None and self.orderBy!="ordenacao":
+            if self.orderBy == "level":
+                self.retorno_query = sorted(self.retorno_query, key=lambda m: m.level)
+            else:
+                self.retorno_query = sorted(self.retorno_query, key=lambda m: m.frequencia)
+        return self.retorno_query
+
     def get(self, request, *args, **kwargs):
+        print("estou no get")
         if request.method == "GET":
             print("estou no get")
-            print(request.GET.get('envName'))
-            print(request.GET.get('orderBy'))
-            # print(request.GET.get())
-        # print(Event.objects.values('agent__env').distinct())
+            self.env = request.GET.get('envName')
+            self.orderBy = request.GET.get('orderBy')
+            self.search_for = request.GET.get('buscarPor')
+            self.pesquisa_text = request.GET.get('pesquisaText')
+            print("todos os parametros sao")
+            print(self.env)
+            print(self.orderBy)
+            print(self.search_for)
+            print(self.pesquisa_text)
+
         return super(EventFilter, self).get(request, *args, **kwargs)
 
 class EventDetail(DetailView):

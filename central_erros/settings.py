90a077c6266528959a46dd137227493816c0ef69
Lisiane da Rosa Morais <lisiane.morais@meta.com.br>
2019-10-28 21:32:22 -0300

    create token

--- central_erros/settings.py
+++ central_erros/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'events',
+    #App de terceiros
     'rest_framework',
+    'rest_framework.authtoken',
+    #nossos apps
+    'events'
 ]
 
 MIDDLEWARE = [

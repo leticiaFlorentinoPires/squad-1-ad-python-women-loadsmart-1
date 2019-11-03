48de4e7118fa29aa29ba1dbd91ce67b5e90b65ea
Lisiane da Rosa Morais <lisiane.morais@meta.com.br>
2019-11-03 19:59:36 -0200

    removed url

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,4 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    #path('password_reset/', views.forgotPassword, name='password_reset'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 
-
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
 from django.contrib.auth.models import User
 from account.forms import SignUpForm
 
 
 # Create your views here.
-class forgotPassword(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/password_reset_form'
-
 def signup(request):
     form = SignUpForm(request.POST)
     if request.method == 'POST':
